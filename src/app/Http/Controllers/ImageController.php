fab453d433c9c6c3146e039cfd46f2d93d770772
Eugene Che <indexpro24@gmail.com>
2024-03-17 22:31:18 +0300

    c

---
+++ src/app/Http/Controllers/ImageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+
+class ImageController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Проверка наличия файла 'image' и заголовка 'Project'
+        if (!$request->hasFile('image') || !$request->header('Project')) {
+            // Возвращаем ошибку, если что-то отсутствует
+            return response()->json(['error' => 'Необходим файл и заголовок Project'], 400);
+        }
+
+        try {
+            // Определение проекта из заголовка
+            $projectHeaderValue = $request->header('Project');
+
+            // Назначение пути до целевой папки в бакете
+            $path = $request->file('image')->store($projectHeaderValue, config('filesystems.default'));
+
+            // Запись в бакет с публичными правами
+            Storage::disk(config('filesystems.default'))->setVisibility($path, 'public');
+
+            // Возврат хэша файла для записи в БД
+            return basename($path);
+        } catch (\Exception $e) {
+            \Log::error('Ошибка при сохранении файла: ' . $e->getMessage());
+            return response()->json(['error' => 'Ошибка при сохранении файла'], 500);
+        }
+    }
+}
